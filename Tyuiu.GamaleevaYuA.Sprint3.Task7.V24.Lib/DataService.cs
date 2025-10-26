b9b95ce1d96f52020004b0d0d4f96ffe1c5e9c17
gamaleeva <gamaleevacin@mail.ru>
2025-10-26 06:29:42 +0500

    Task 7

---
+++ Tyuiu.GamaleevaYuA.Sprint3.Task7.V24.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.GamaleevaYuA.Sprint3.Task7.V24.Lib
+{
+    public class DataService : ISprint3Task7V24
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = stopValue - startValue + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round(Math.Sin(x) / (x + 1.2) - Math.Sin(x) * 2 - 2 * x, 2);
+                valueArray[count] = y;
+                if ((x + 1.2) == 0)
+                {
+                    valueArray[count] = 0;
+                }
+                count++;
+            }
+            return valueArray;
+        }
+    }
+}
--- Tyuiu.GamaleevaYuA.Sprint3.Task7.V24/Program.cs
+++ Tyuiu.GamaleevaYuA.Sprint3.Task7.V24/Program.cs
@@ -1,4 +1,4 @@
-﻿using Tyuiu.GamaleevaYuA.Sprint3.Task6.V14.Lib;
+﻿using Tyuiu.GamaleevaYuA.Sprint3.Task7.V24.Lib;
 internal class Program
 {
     private static void Main(string[] args)
@@ -7,26 +7,36 @@ internal class Program
         Console.Title = "Спринт #3 | Выполнила: Гамалеева Ю. А. | ИБКСб-25-1";
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* Спринт #3                                                               *");
-        Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
-        Console.WriteLine("* Задание #6                                                              *");
-        Console.WriteLine("* Вариант #14                                                             *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #24                                                             *");
         Console.WriteLine("* Выполнила: Гамалеева Юлия Андреевна | ИБКСб-25-1                        *");
         Console.WriteLine("***************************************************************************");
-        Console.WriteLine("* Написать программу, которая ищет среди целых чисел, принадлежащих       *");
-        Console.WriteLine("* числовому отрезку [7, 16] количество всех делителей больше 5            *");
+        Console.WriteLine("* Написать программу, которая выводит таблицу значений функции:           *");
+        Console.WriteLine("* F(x) = sin(x)/(x + 1,2) - sin(x) * 2 - 2x                               *");
         Console.WriteLine("*                                                                         *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
         Console.WriteLine("***************************************************************************");
-        int startValue = 7;
-        int stopValue = 16;
-        Console.WriteLine("Начало отрезка = " + startValue);
-        Console.WriteLine("Конец отрезка = " + stopValue);
+        int startValue = -5;
+        int stopValue = 5;
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+        int len = ds.GetMassFunction(startValue, stopValue).Length;
+        double[] valueArray = ds.GetMassFunction(startValue, stopValue);
+
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
         Console.WriteLine("***************************************************************************");
-        Console.WriteLine("Сумма делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+        Console.WriteLine("+----------+----------+");
+        Console.WriteLine("|    X     |   f(x)   |");
+        Console.WriteLine("+----------+----------+");
+        for (int i = 0; i <= len - 1; i++)
+        {
+            Console.WriteLine("|{0,5:d}     |  {1, 5:f2}   |", startValue, valueArray[i]);
+            startValue++;
+        }
+        Console.WriteLine("+----------+----------+");
         Console.ReadKey();
-
     }
 }
